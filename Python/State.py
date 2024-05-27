b1a4df805ccc8bc521ea0110461ccf356f2b1115
Alex <alejandro.rod.lop@gmail.com>
2024-05-27 10:01:23 +0200

    Moved state methods into class

--- Python/State.py
+++ Python/State.py
@@ -1,4 +1,4 @@
-from typing import List, Iterator, cast
+from typing import List, Iterator, cast, Self
 
 from DataFreezer import freeze_data
 
@@ -7,11 +7,105 @@ class State:
 
     def __init__(
             self,
+            jobs: List[List[int]],
             schedule: List[List[int]],
             workers_status: List[int]
     ):
+        self.jobs = jobs
         self.schedule = schedule
         self.workers_status = workers_status
+
+    def calculate_h_cost(self) -> int:
+        h_costs: List[int] = []
+        for job_idx, job in enumerate(self.schedule):
+            h_costs.append(0)
+            for task_idx, task in enumerate(job):
+                if (task == -1):
+                    h_costs[job_idx] = (
+                        h_costs[job_idx] +
+                        self.jobs[job_idx][task_idx]
+                    )
+        return min(h_costs)
+
+    def is_goal_state(self) -> bool:
+        for job in self.schedule:
+            for task in job:
+                if (task == -1):
+                    return False
+        return True
+
+    def get_first_unscheduled_tasks_idxs(self) -> List[int]:
+        first_unscheduled_tasks_idxs: List[int] = []
+        for job in self.schedule:
+            current_task_idx: int = 0
+            while (
+                current_task_idx < len(job) and
+                job[current_task_idx] >= 0
+            ):
+                current_task_idx = current_task_idx + 1
+            if (current_task_idx < len(job)):
+                first_unscheduled_tasks_idxs.append(current_task_idx)
+            else:
+                first_unscheduled_tasks_idxs.append(-1)
+        return first_unscheduled_tasks_idxs
+
+    def get_first_unscheduled_tasks_start_times(
+        self,
+        first_unscheduled_tasks_idxs: List[int],
+    ) -> List[int]:
+        first_unscheduled_tasks_start_times: List[int] = []
+        for job_idx, job in enumerate(self.schedule):
+            currently_unscheduled_task_idx = (
+                first_unscheduled_tasks_idxs[job_idx]
+            )
+            if (currently_unscheduled_task_idx != -1):
+                if (currently_unscheduled_task_idx == 0):
+                    first_unscheduled_tasks_start_times.append(0)
+                else:
+                    first_unscheduled_tasks_start_times.append(
+                        self.jobs[job_idx][currently_unscheduled_task_idx - 1]
+                        + job[currently_unscheduled_task_idx - 1]
+                    )
+            else:
+                first_unscheduled_tasks_start_times.append(-1)
+        return first_unscheduled_tasks_start_times
+
+    def get_neighbors_of(self) -> List[Self]:
+        neighbors: List[State] = []
+
+        first_unscheduled_tasks_idxs = self.get_first_unscheduled_tasks_idxs()
+        first_unscheduled_tasks_start_times = (
+            self.get_first_unscheduled_tasks_start_times(
+                first_unscheduled_tasks_idxs
+            )
+        )
+
+        for job_idx in range(0, len(self.schedule), 1):
+            task_start_time = first_unscheduled_tasks_start_times[job_idx]
+            if (task_start_time != -1):
+                unscheduled_task_idx = first_unscheduled_tasks_idxs[job_idx]
+                for worker_id, worker_start_free_time in (
+                    enumerate(self.workers_status)
+                ):
+                    worker_start_time = max(
+                        worker_start_free_time,
+                        task_start_time
+                    )
+
+                    new_schedule = [job[:] for job in self.schedule]
+                    new_schedule[job_idx][unscheduled_task_idx] = (
+                        worker_start_time
+                    )
+
+                    new_workers_status = self.workers_status[:]
+                    new_workers_status[worker_id] = (
+                        worker_start_time +
+                        self.jobs[job_idx][unscheduled_task_idx]
+                    )
+
+                    neighbors.append(State(new_schedule, new_workers_status))
+
+        return neighbors
 
     def __iter__(self) -> Iterator[List[int]]:
         return self.schedule.__iter__()
--- Python/main.py
+++ Python/main.py
@@ -4,106 +4,6 @@ from State import State
 from Wrappers import timeit
 
 
-def calculate_h_cost(state: State, jobs: List[List[int]]) -> int:
-    h_costs: List[int] = []
-    for job_idx, job in enumerate(state):
-        h_costs.append(0)
-        for task_idx, task in enumerate(job):
-            if (task == -1):
-                h_costs[job_idx] = h_costs[job_idx] + jobs[job_idx][task_idx]
-    return min(h_costs)
-
-
-def is_goal_state(state: State) -> bool:
-    for job in state:
-        for task in job:
-            if (task == -1):
-                return False
-    return True
-
-
-def get_first_unscheduled_tasks_idxs(state: State) -> List[int]:
-    first_unscheduled_tasks_idxs: List[int] = []
-    for job in state:
-        current_task_idx: int = 0
-        while (
-            current_task_idx < len(job) and
-            job[current_task_idx] >= 0
-        ):
-            current_task_idx = current_task_idx + 1
-        if (current_task_idx < len(job)):
-            first_unscheduled_tasks_idxs.append(current_task_idx)
-        else:
-            first_unscheduled_tasks_idxs.append(-1)
-    return first_unscheduled_tasks_idxs
-
-
-def get_first_unscheduled_tasks_start_times(
-    state: State,
-    first_unscheduled_tasks_idxs: List[int],
-    jobs: List[List[int]]
-) -> List[int]:
-    first_unscheduled_tasks_start_times: List[int] = []
-    for job_idx, job in enumerate(state):
-        currently_unscheduled_task_idx = first_unscheduled_tasks_idxs[job_idx]
-        if (currently_unscheduled_task_idx != -1):
-            if (currently_unscheduled_task_idx == 0):
-                first_unscheduled_tasks_start_times.append(0)
-            else:
-                first_unscheduled_tasks_start_times.append(
-                    jobs[job_idx][currently_unscheduled_task_idx - 1] +
-                    job[currently_unscheduled_task_idx - 1]
-                )
-        else:
-            first_unscheduled_tasks_start_times.append(-1)
-    return first_unscheduled_tasks_start_times
-
-
-def get_neighbors_of(state: State, jobs: List[List[int]]) -> List[State]:
-    neighbors: List[State] = []
-
-    first_unscheduled_tasks_idxs = get_first_unscheduled_tasks_idxs(state)
-    first_unscheduled_tasks_start_times = (
-        get_first_unscheduled_tasks_start_times(
-            state,
-            first_unscheduled_tasks_idxs,
-            jobs
-        )
-    )
-
-    for job_idx in range(0, len(state), 1):
-        task_start_time = first_unscheduled_tasks_start_times[job_idx]
-        if (task_start_time != -1):
-            unscheduled_task_idx = first_unscheduled_tasks_idxs[job_idx]
-            for worker_id, worker_start_free_time in (
-                enumerate(state.workers_status)
-            ):
-                worker_start_time = max(
-                    worker_start_free_time,
-                    task_start_time
-                )
-
-                new_schedule = [job[:] for job in state]
-                new_schedule[job_idx][unscheduled_task_idx] = worker_start_time
-
-                new_workers_status = state.workers_status[:]
-                new_workers_status[worker_id] = (
-                    worker_start_time +
-                    jobs[job_idx][unscheduled_task_idx]
-                )
-
-                neighbors.append(State(new_schedule, new_workers_status))
-
-    return neighbors
-
-
-def distance(a: State, b: State, jobs: List[List[int]]) -> int:
-    for job_idx in range(0, len(a), 1):
-        for task_idx in range(0, len(a[job_idx]), 1):
-            if (a[job_idx][task_idx] != b[job_idx][task_idx]):
-                return jobs[job_idx][task_idx]
-
-
 @timeit
 def a_star(jobs: List[List[int]], n_workers: int) -> List[List[int]]:
     starting_state: State = State(
@@ -115,7 +15,7 @@ def a_star(jobs: List[List[int]], n_workers: int) -> List[List[int]]:
         starting_state: 0
     }
     f_costs: Dict[State, int] = {
-        starting_state: calculate_h_cost(starting_state, jobs)
+        starting_state: starting_state.calculate_h_cost()
     }
 
     open_set: SortedList[State] = SortedList(
@@ -125,9 +25,9 @@ def a_star(jobs: List[List[int]], n_workers: int) -> List[List[int]]:
 
     while len(open_set) > 0:
         current_state: State = open_set.pop()
-        if (is_goal_state(current_state)):
+        if (current_state.is_goal_state()):
             return current_state.schedule
-        neighbor_states: List[State] = get_neighbors_of(current_state, jobs)
+        neighbor_states: List[State] = current_state.get_neighbors_of()
         for neighbor in neighbor_states:
             tentative_g_cost: int = max(neighbor.workers_status)
             if (
@@ -137,14 +37,16 @@ def a_star(jobs: List[List[int]], n_workers: int) -> List[List[int]]:
                 g_costs[neighbor] = tentative_g_cost
                 f_costs[neighbor] = (
                     tentative_g_cost +
-                    calculate_h_cost(neighbor, jobs)
+                    neighbor.calculate_h_cost()
                 )
                 if neighbor not in open_set:
                     open_set.append(neighbor)
 
 
-def process_jobs(jobs: List[List[int]]) -> None:
-    for n_workers in range(1, len(jobs)+1, 1):
+def process_jobs(jobs: List[List[int]], max_workers: int = -1) -> None:
+    if (max_workers == -1):
+        max_workers = len(jobs) + 1
+    for n_workers in range(1, max_workers+1, 1):
         a_star(jobs, n_workers)
 
 
@@ -160,13 +62,13 @@ def main():
 
     process_jobs([[2, 5], [3, 3], [1, 7]])
     process_jobs([[2, 5, 1], [3, 3, 3], [1, 7, 2]])
-    process_jobs([[2, 5, 1, 2], [3, 3, 3, 7], [1, 7, 2, 8]])
-    process_jobs([[2, 5, 1, 2, 5], [3, 3, 3, 7, 5], [1, 7, 2, 8, 1]])
+    process_jobs([[2, 5, 1, 2], [3, 3, 3, 7], [1, 7, 2, 8]], 2)
+    process_jobs([[2, 5, 1, 2, 5], [3, 3, 3, 7, 5], [1, 7, 2, 8, 1]], 1)
 
     process_jobs([[2, 5], [3, 3], [1, 7], [2, 2]])
-    process_jobs([[2, 5, 1], [3, 3, 3], [1, 7, 2], [2, 2, 3]])
-    process_jobs([[2, 5, 1, 2], [3, 3, 3, 7], [1, 7, 2, 8], [2, 2, 3, 6]])
-    process_jobs([[2, 5, 1, 2, 5], [3, 3, 3, 7, 5], [1, 7, 2, 8, 1], [2, 2, 3, 6, 4]])
+    process_jobs([[2, 5, 1], [3, 3, 3], [1, 7, 2], [2, 2, 3]], 2)
+    process_jobs([[2, 5, 1, 2], [3, 3, 3, 7], [1, 7, 2, 8], [2, 2, 3, 6]], 1)
+    process_jobs([[2, 5, 1, 2, 5], [3, 3, 3, 7, 5], [1, 7, 2, 8, 1], [2, 2, 3, 6, 4]], 1)
 
 
 if __name__ == '__main__':
