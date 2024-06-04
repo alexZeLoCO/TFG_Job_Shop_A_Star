40b11af0ec7ff10f7794edb931249c9d3a2bab30
Alex <alejandro.rod.lop@gmail.com>
2024-06-04 15:54:04 +0200

    Added makespan

--- Python/Wrappers.py
+++ Python/Wrappers.py
@@ -1,6 +1,8 @@
 from functools import wraps
 import time
 
+from State import State
+
 
 def timeit(func):
     @wraps(func)
@@ -10,19 +12,21 @@ def timeit(func):
         n_workers: int = args[1]
         func(*args, **kwargs)
         start_time = time.perf_counter()
-        result = func(*args, **kwargs)
+        result: State = func(*args, **kwargs)
         end_time = time.perf_counter()
         total_time = end_time - start_time
         print(
             (f"py;1;{func.__name__};{args};{n_jobs};{n_tasks};"
-             f"{n_workers};{total_time:1.5E};{result}")
+             f"{n_workers};{total_time:1.5E};{result.schedule};"
+             f"{max(result.workers_status)}")
         )
         return result
     return timeit_wrapper
 
-    
+
 def singleton(class_):
     instances = {}
+
     def getinstance(*args, **kwargs):
         if class_ not in instances:
             instances[class_] = class_(*args, **kwargs)
